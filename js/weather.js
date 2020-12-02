deb229fcd92405f56521f84410db10612d606a1e
Matt Powers <mattpowers@Matts-MacBook-Air.local>
2020-12-02 17:45:15 -0500

    Finished homework 7

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,30 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.getElementById("forecast").style.display="block";
+
 
     //Set default location if one isn't provided
-    let location;
+    let location = "Ann Arbor";
+    if (document.querySelector("#location").value != ""){
+         location = document.querySelector("#location").value;
+    }
+    
 
     // Your code here.
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    let fomat;
+    let format = "imperial";
+    if (document.querySelector("input[name = temp]:checked")){
+        format = document.querySelector("input[name = temp]:checked").value
+    }
 
     // Your code here.
     console.log("Format is " + format);
 
     //set the query  
-    let query;
+    let query = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units="+ format + "&appid=112a93a1ea9156caf2fa8783e0392578";
     // Your code here.  
     
     console.log("Query is :" + query);
@@ -23,10 +32,11 @@ function gettingJSON(){
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
 
-    let loc;
-    let temp;
-    let tempImg;
+    let loc = document.querySelector("#loc");
+    let temp = document.querySelector("#temp");
+    let tempImg = document.querySelector("#tempImg");
     // Your code here.
+    
 
 
     $.getJSON(query,function(json){
@@ -34,6 +44,11 @@ function gettingJSON(){
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
+        console.log(json)
+        loc.innerHTML = json["name"]
+        temp.innerHTML = json["main"]["temp"]
+        tempImg.src = "http://openweathermap.org/img/wn/" + json["weather"][0]["icon"] + ".png"
+        tempImg.alt = "Image of Weather"
 
     });
 }
